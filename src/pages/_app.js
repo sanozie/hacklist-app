949166039c5f3a73b58e0f1a7204be6b234dc7da
Samuel Anozie <sanoziec@gmail.com>
2021-07-20 22:46:39 -0500

    Global Initial Props

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,27 @@
 import React from 'react'
+import App from 'next/app'
+
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/mixins.scss';
 import 'styles/variables.scss';
 import 'styles/index.scss';
-
-// Theme
-
-import theme from 'lib/AppTheme'
-
 //calendar styles
 import 'react-calendar/dist/Calendar.css';
 
 import { Store } from 'store'
 
-export default function MyApp({ Component, pageProps }) {
+function Application({ Component, pageProps }) {
     return (
         <Store>
             <Component {...pageProps} />
         </Store>
     )
 }
+
+Application.getInitialProps = async appContext => {
+    const appProps = await App.getInitialProps(appContext)
+
+    return { ...appProps }
+}
+export default Application
